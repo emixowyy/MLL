619d35a971ad26d6fd65b7e997fad88399f9f81e
emixowyy <emix.rowerek.lubie@gmail.com>
2026-02-16 10:46:19 +0100

    It works but is shit
    gotta fix so works better

--- books_select.php
+++ books_select.php
@@ -11,22 +11,22 @@ $nresults = $_GET['nresults'] ?? 5;
 
 
 if (isset($title)) {
-    $dbquery = "SELECT * FROM books WHERE title like ? LIMIT $nresults";
+    $dbquery = "SELECT authors.name, books.* FROM books JOIN authors ON books.aid = authors.aid WHERE title like ? LIMIT $nresults";
     $param = ["%$title%"];
     $queryparam = urlencode("intitle:$title");
 }
 elseif (isset($isbn)) {
-    $dbquery = "SELECT * FROM books WHERE isbn = ? LIMIT $nresults";
-    $param = ["%$isbn%"];
+    $dbquery = "SELECT authors.name, books.* FROM books JOIN authors ON books.aid = authors.aid WHERE isbn = ? LIMIT $nresults";
+    $param = [$isbn];
     $queryparam = urlencode("isbn:$isbn");
 }
 elseif (isset($author)) {
-    $dbquery = "SELECT books.* FROM books JOIN authors ON books.aid = authors.aid WHERE authors.name LIKE ? LIMIT $nresults";
+    $dbquery = "SELECT authors.name, books.* FROM books JOIN authors ON books.aid = authors.aid WHERE authors.name LIKE ? LIMIT $nresults";
     $param = ["%$author%"];
     $queryparam = urlencode("inauthor:$author");
 }
 elseif (isset($categories)) {
-    $dbquery = "SELECT * FROM books WHERE categories like ? LIMIT $nresults";
+    $dbquery = "SELECT authors.name, books.* FROM books JOIN authors ON books.aid = authors.aid WHERE categories like ? LIMIT $nresults";
     $param = ["%$categories%"];
     $queryparam = urlencode("subject:$categories");
 }
@@ -36,52 +36,58 @@ $dbqueryexec->execute($param);
 $queryresult = $dbqueryexec->fetchALL(PDO::FETCH_ASSOC);
 
 if (count($queryresult) === 0 ) {
-    $url = curl_init("https://www.googleapis.com/books/v1/volumes/?q=$queryparam&maxResults=$nresults&printType=books&orderBy=relevance&projection=lite");
+    $url = curl_init("https://www.googleapis.com/books/v1/volumes/?q=$queryparam&maxResults=10&printType=books&orderBy=relevance");
     curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
     $getbooks = curl_exec($url);
     curl_close($url);
     $books = json_decode($getbooks, true);
+    foreach ($books as $book) {
 
-
-    function selectBestBook($items) {
-        $bestBook = null;
-        $bestScore = 0;
-
-        foreach ($items as $book) {
-            $info = $book['volumeInfo'] ?? [];
-            $score = 0;
-
-            if (!empty($info['title'])) $score += 5;
-            if (!empty($info['authors'][0])) $score += 5;
-            if (!empty($info['publishedDate'])) $score += 3;
-
-            if (!empty($info['industryIdentifiers'])) {
-                foreach ($info['industryIdentifiers'] as $id) {
-                    if ($id['type'] === 'ISBN_13') {
-                        $score += 5;
-                        break;
-                    }
-                }
-            }
-            if (!empty($info['description'])) $score += 2;
-            if (!empty($info['categories'])) $score += 2;
-            if (!empty($info['imageLinks']['thumbnail'])) $score += 2;
-            if (!empty($info['averageRating'])) $score += 1;
-            if (!empty($info['language'])) $score += 1;
-
-            if ($score > $bestScore) {
-                $bestScore = $score;
-                $bestBook = $book;
-            }
-        }
-
-        return $bestBook;
     }
+    // function selectBestBook($items) {
+    //     $bestBook = null;
+    //     $bestScore = 0;
 
-    $info = $bestBook['volumeInfo'] ?? [];
+    //     foreach ($items as $book) {
+    //         $info = $book['volumeInfo'] ?? [];
+    //         $score = 0;
 
-    $titlein       = $info['title'] ?? null;
-    $authorin      = $info['authors'][0] ?? null;
+    //         if (!empty($info['title'])) $score += 5;
+    //         if (!empty($info['authors'][0])) $score += 5;
+    //         if (!empty($info['publishedDate'])) $score += 3;
+
+    //         if (!empty($info['industryIdentifiers'])) {
+    //             foreach ($info['industryIdentifiers'] as $id) {
+    //                 if ($id['type'] === 'ISBN_13') {
+    //                     $score += 5;
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //         if (!empty($info['description'])) $score += 2;
+    //         if (!empty($info['categories'])) $score += 2;
+    //         if (!empty($info['imageLinks']['thumbnail'])) $score += 2;
+    //         if (!empty($info['averageRating'])) $score += 1;
+    //         if (!empty($info['language'])) $score += 1;
+
+    //         if ($score > $bestScore) {
+    //             $bestScore = $score;
+    //             $bestBook = $book;
+    //         }
+    //     }
+
+    //     return $bestBook;
+    // }
+    // $bestBook = selectBestBook($books['items']);
+    // if (empty($books['items'])) {
+    // echo json_encode(["success" => false, "message" => "No books found"]);
+    // exit;
+    // }
+
+    $info = $bestBook['volumeInfo'];
+
+    $titlein       = $info['title'];
+    $authorin      = $info['authors'][0];
     $publisheddate = $info['publishedDate'] ?? null;
     $maturity      = $info['maturityRating'] ?? null;
     $description   = $info['description'] ?? null;
@@ -103,17 +109,17 @@ if (count($queryresult) === 0 ) {
         }
     }
 
-    $insertbooks = $pdo->prepare("INSERT INTO books (aid,isbn,title,pub_date,b_desc,categories,maturity,rating,language,thumbnail) VALUES (?,?,?,?,?,?,?,?,?,?)");
+    $insertbooks = $pdo->prepare("INSERT IGNORE INTO books (aid,isbn,title,pub_date,b_desc,categories,maturity,rating,language,thumbnail) VALUES (?,?,?,?,?,?,?,?,?,?)");
     $checkauthor = $pdo->prepare("SELECT aid FROM authors WHERE name = ?");
     $checkauthor->execute([$authorin]);
     $authorid = $checkauthor->fetch(PDO::FETCH_ASSOC);
-    if ($authorid) {
-        $aid = $authorid['aid'];
-    }
-    else {
+    if ($authorid == false) {
         $insertauthor = $pdo->prepare("INSERT INTO authors (name) VALUES (?)");
         $insertauthor->execute([$authorin]);
         $aid = $pdo->lastInsertId();
+    }
+    else {
+        $aid = $authorid['aid'];
     }
     $insertbooks->execute([
         $aid,
@@ -121,15 +127,19 @@ if (count($queryresult) === 0 ) {
         $titlein,
         $publisheddate,
         $description,
-        is_array($categoriesin) ? implode(',', $categoriesin) : null,
+        is_array($categoriesin) ? json_encode($categoriesin) : null,
         $maturity,
         $averagerating,
         $language,
         $thumbnail
     ]);
 
-    $anotherquerry = $pdo->prepare("SELECT * FROM books WHERE title = ?");
-    $anotherquerry -> execute([$titlein]);
+
+
+
+
+    $anotherquerry = $pdo->prepare("SELECT authors.name,books.* FROM books JOIN authors ON books.aid WHERE books.title LIKE ?");
+    $anotherquerry -> execute(["%$titlein%"]);
     $anotherquerryresult = $anotherquerry->fetch(PDO::FETCH_ASSOC);
     echo json_encode(["success: " => true, "Data: " => $anotherquerryresult]);
 }
